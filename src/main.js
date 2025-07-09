57640f45e3b3ec1449e94161a46cc0115f8cd0ce
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2025-07-09 16:10:43 -0300

    Atualiza metadados e estrutura do HTML, adiciona informações de contato e estilização ao CSS.

--- src/main.js
+++ src/main.js
@@ -1,2 +1,19 @@
-import './style.css'
+// import './style.css'
 console.log('Hello World! This is the main.js file of your project.');
+
+// Seleciona todos os botões dentro da div com a classe contact-info
+const contactButtons = document.querySelectorAll('.contact-info button');
+
+// Função para vibrar o dispositivo
+function darFeedbackHaptico() {
+  if ('vibrate' in navigator) {
+    navigator.vibrate(50); // vibra por 50ms
+  }
+}
+
+// Adiciona o evento de clique a cada botão
+contactButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    darFeedbackHaptico();
+  });
+});
